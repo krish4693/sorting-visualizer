d17acc026c29c2f9939efa0b021001ea2f4ee017
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-05-10 19:51:02 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items- justify-around'>
+        <p>Generate New Array</p>
+        <p>Change Array Size</p>
+        <p>Merge Sort</p>
+        <p>Sort</p>
+    </div>
+  )
+}
+
+export default Navbar
